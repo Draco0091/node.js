49b432b2299c5ceb0130ed11dffefd70228a6774
Mohammed Umar <umarjkhan16@gmail.com>
2024-10-18 12:01:41 -0400

    Update app.js

--- nodejs-website-main/app.js
+++ nodejs-website-main/app.js
@@ -20,4 +20,14 @@ http.createServer(function (req, res) {
 
     fs.readFile(filename, function(err, data) {
         if (err) {
-            res.writeHead(404, {'Content-Type': 'text/htm
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        } 
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+
+}).listen(process.env.PORT || 8080, function() {
+    console.log("Server is listening on port " + (process.env.PORT || 8080));
+});
