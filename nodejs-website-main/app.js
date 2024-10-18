2d5fd117619f5ac029df17a359d2d5b64faa1c44
Mohammed Umar <umarjkhan16@gmail.com>
2024-10-18 12:08:10 -0400

    Update app.js

--- nodejs-website-main/app.js
+++ nodejs-website-main/app.js
@@ -1,33 +1,10 @@
 var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var path = require('path');
 
 http.createServer(function (req, res) {
-
-    var q = url.parse(req.url, true);
-    var filename = "." + q.pathname;
-
-    // If the root URL is accessed, serve the index.html file
-    if (q.pathname === "/") {
-        filename = "./index.html";
-    }
-
-    // Append .html extension if no extension is provided
-    if (!path.extname(filename)) {
-        filename += ".html";
-    }
-
-    fs.readFile(filename, function(err, data) {
-        if (err) {
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            return res.end("404 Not Found");
-        } 
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    });
-
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<h1>Hello World</h1>');
+    res.end();
 }).listen(process.env.PORT || 8080, function() {
     console.log("Server is listening on port " + (process.env.PORT || 8080));
 });
+
